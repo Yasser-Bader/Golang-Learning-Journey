d64bac69979e122b185547fb1d09cc06ebcd1509
Yasser Bader <67002064+Yasser-Bader@users.noreply.github.com>
2025-12-15 20:08:37 +0200

    feat: add variables and constants example
    Added main.go covering variable declarations (var vs short syntax) and constants

--- 01-Basics/Variables/main.go
+++ 01-Basics/Variables/main.go
@@ -1,1 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+    
+	var firstName string = "Ahmed"
+	var age = 26 
+
+  // Short Declaration
+	jobTitle := "Backend Developer"
+	salary := 5000.50
+	isHired := true
+
+	//Constants
+	const CompanyName = "Tech Corp"
+
+	// print data 
+	fmt.Println("=== Employee System ===")
+	fmt.Println("Company:", CompanyName)
+	fmt.Println("Name:", firstName)
+	fmt.Println("Age:", age)
+	fmt.Println("Job:", jobTitle)
+	
+	// Printf   (Formatted Print)
+	// %f معناها رقم عشري، و .2 معناها علامتين بس بعد الصفر
+	fmt.Printf("Salary: %.2f EGP\n", salary) 
+	fmt.Println("Hired Status:", isHired)
+}
 
