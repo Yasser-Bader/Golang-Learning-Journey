43a55c0dcea771cf95351f5e9167b5f310a3a246
Yasser Bader <67002064+Yasser-Bader@users.noreply.github.com>
2025-12-19 15:46:48 +0000

    feat:print High temperature days using array and slice

--- 03-ArraysSlices/01-Arrays/main.go
+++ 03-ArraysSlices/01-Arrays/main.go
@@ -1,1 +1,18 @@
+// طباعة درجة الحراره المرتفعه فقط
 package main
+
+import "fmt"
+
+func main() {
+	array := [5]float64{30.0, 42.5, 28.0, 40.5, 35.5}
+	var highTemps []float64
+
+	for _, v := range array {
+		if v >= 40.0 {
+			highTemps = append(highTemps, v)
+		}
+	}
+
+	fmt.Println("High temperature days:", highTemps)
+
+}
